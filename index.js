2990ff7cbeb9f1cd43ebadfca2a159518c24b488
coker2019 <olabisi2k11@gmail.com>
2023-04-17 10:06:33 +0100

    Add Mobile-menu

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+
+
+const hamburgerButton = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-lists');
+const closed = document.querySelector('.close-icon');
+const mobileList = document.querySelectorAll('.mobile-list');
+
